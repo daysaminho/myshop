94095ca38a3b698c89f8fd88fadb550bf324bd61
daysaminho <daysam.moussaoui@epitech.digital>
2024-11-14 12:33:53 +0100

    Delete data.js

--- data.js
+++
@@ -1,8 +0,0 @@
-// data.js
-const users = [
-  { username: 'admin', password: 'admin', role: 'admin' }
-];
-
-let currentUser = null; // Variable pour l'utilisateur actuellement connecté
-
-module.exports = { users, currentUser };
