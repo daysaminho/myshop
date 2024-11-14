2310829e83fe9a268d2c41581550feb09d376219
FabienBoscher <fabien.boscher@epitech.digital>
2024-11-14 17:47:00 +0100

    Delete server directory

--- server/routes/debugRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const data = require('../data');
-const router = express.Router();
-
-router.get('/users', (req, res) => {
-    res.json(data.users);  // Affiche tous les utilisateurs en mémoire
-});
-
-module.exports = router;
